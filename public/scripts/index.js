b9d2cae1bcb98a537dd1491bbd6276fe3fc8f96e
rundlof <12802889+rundlof@users.noreply.github.com>
2021-01-12 19:28:59 +0100

    updates

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,48 +3,11 @@ adk.cmd.push(function() {
     var advisibleID = '10122804'
 
     adk.config()
-        .addBreakpoint('mobile', 125)
-        .addBreakpoint('tablet', 768)
-        .addBreakpoint('desktop', 1025)
-
         .addTemplate('native-teaser', adk.template.url((window.PUBLIC_ROOT || '') + '/templates/native-teaser.html'))
-
-        .addContainer('native-teaser', adk.container.config()
-            .abstract()
-            // .mode('dev').formats([300,300])
-            .provider('adk.native.teaser'))
-
         .addContainer('native-teaser-desktop', adk.container.config()
-            .extends('native-teaser')
-            .breakpoints('mobile', 'desktop')
-            // .param('category')
-            // .param('props', {
-            //     'device': 'desktop',
-            // })
-            // .replace()
+            .provider('adk.native.teaser')
             .template('native-teaser')
-            // .templateValues({
-            //     foo: 'bar',
-            // })
             .params('aid', advisibleID))
-            // .params('keyValues', {
-            //     d: 4,
-            // }))
-            // aspectRatio: 270 / 115,
-            // allowFallback: true,
-            // aspectRatioVariance: 2,
-
-        // .addContainer('native-teaser-mobile', adk.container.config()
-        //     .extends('native-teaser')
-        //     .breakpoints('mobile')
-        //     // .param('tag', 'mobile')
-        //     .params('aid', advisibleID)
-        //     .template('native-teaser')
-        //     .params('_accept', {
-        //         aspectRatio: 270 / 115,
-        //         allowFallback: true,
-        //     }))
-
         .apply()
         .init()
 
